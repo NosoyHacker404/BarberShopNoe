42374d8d3cd047b7d1723612311bb6167ffbda1a
NosoyHacker404 <Anonymous_26s@hotmail.com>
2020-10-16 15:51:26 -0700

    Add files via upload

--- js/form.js
+++ js/form.js
@@ -6,7 +6,7 @@ document.querySelector('#submit').addEventListener('click',function(){
     let barbero = document.querySelector('#barbero').value;
     let servicio = document.querySelector('#servicio').value;
 
-    let url = "https://api.whatsapp.com/send?phone=526411020082&text=*_Barberia Moiza_*%0A*Reservas*%0A%0A*¿Cual es tu nombre?*%0A" + cliente + "%0A*Indica la fecha de tu reserva*%0A" + fecha + "%0A*Indica la hora de tu reserva*%0A" + hora + "%0A*Barbero de preferencia*%0A" + barbero + "%0A*¿Cual es el servicio que se desea realizar?*%0A" + servicio;
+    let url = "https://api.whatsapp.com/send?phone=526411020822&text=*_Barberia Noe_*%0A*Reservas*%0A%0A*¿Cual es tu nombre?*%0A" + cliente + "%0A*Indica la fecha de tu reserva*%0A" + fecha + "%0A*Indica la hora de tu reserva*%0A" + hora + "%0A*Barbero de preferencia*%0A" + barbero + "%0A*¿Cual es el servicio que se desea realizar?*%0A" + servicio;
     window.open(url);
 
 });
